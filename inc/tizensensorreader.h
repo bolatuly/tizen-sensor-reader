76dc83296d82ceb4fa2779df396d284a6b359965
azamat <b.a.bolatuly@gmail.com>
2018-10-11 13:38:58 +0900

    basic ui

---
+++ inc/tizensensorreader.h
@@ -0,0 +1,19 @@
+#ifndef __tizensensorreader_H__
+#define __tizensensorreader_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "tizensensorreader"
+
+#if !defined(PACKAGE)
+#define PACKAGE "github.stem.tizensensorreader"
+#endif
+
+#endif /* __tizensensorreader_H__ */
